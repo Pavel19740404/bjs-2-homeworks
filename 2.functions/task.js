be3ef0e0e95cc5cba85aaa95811af38d72285027
Pavel19740404 <74202355+Pavel19740404@users.noreply.github.com>
2024-07-04 15:17:52 +0300

    Update task2.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -20,6 +20,66 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  };
+  const initialValue = 0;
+  const sumWithInitial = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+  let sum = sumWithInitial;
+  return sum; 
+}
+
+function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  };
+  let min = arr[0];
+  let max = arr[0];
+  for (let num of arr) {
+    if (num > max) {
+        max = num;
+    }
+    if (num < min) {
+        min = num;
+    }
+  };
+  return (max - min);
+}
+
+function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      sumEvenElement += num;
+    } else {
+      sumOddElement += num;
+    }
+  }
+  return (sumEvenElement - sumOddElement);
+}
+
+function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      sumEvenElement += num;
+      countEvenElement += 1;
+    }
+  }
+  return (sumEvenElement / countEvenElement);
+}
+function summElementsWorker(...arr) {
 
 }
 
