7009e3a263a2c841b3b8f474626c79acedf5dcd6
Pavel19740404 <74202355+Pavel19740404@users.noreply.github.com>
2024-07-04 15:13:51 +0300

    Update task1.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,21 @@
 function getArrayParams(...arr) {
-
+  let min = arr[0];
+  let max = arr[0];
+  for (let num of arr) {
+    if (num > max) {
+        max = num;
+    }
+    if (num < min) {
+        min = num;
+    }
+  }
+  const initialValue = 0;
+  const sumWithInitial = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+  let sum = sumWithInitial;
+  let avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
